313f295dddcc0f86aea215a21ad92dea7b6af9d0
pruzhan <58399140+pruzhan@users.noreply.github.com>
2021-03-31 17:06:13 +0300

    add javadoc

--- src/main/java/ru/pruzhan/CitingBot/Application.java
+++ src/main/java/ru/pruzhan/CitingBot/Application.java
@@ -4,6 +4,11 @@ package ru.pruzhan.CitingBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Главный класс, содержащий в себе точку входа в приложение
+ * @author Dmitriy Fedrushkov
+ */
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
